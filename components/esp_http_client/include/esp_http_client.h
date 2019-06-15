b1da6358c9d96741adb64c02a4245dba6f476f79
Viet-Trung Luu <vtl@tricot.io>
2019-06-14 21:28:52 -0700

    [esp_http_client][esp_transport] Add modification copyright comments.
    This is required by the Apache 2.0 license.

--- components/esp_http_client/include/esp_http_client.h
+++ components/esp_http_client/include/esp_http_client.h
@@ -11,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// Modifications copyright 2019 Tricot Inc.
 
 #ifndef _ESP_HTTP_CLIENT_H
 #define _ESP_HTTP_CLIENT_H
